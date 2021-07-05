a29ca162b8ab213836278046faef613f9d38a65d
Foldster <foldsters@gmail.com>
2021-07-05 12:12:18 -0400

    Setting up Project

--- app/src/main/java/com/steamtechs/thepedal/MainActivity.kt
+++ app/src/main/java/com/steamtechs/thepedal/MainActivity.kt
@@ -3,6 +3,7 @@ package com.steamtechs.thepedal
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// MainActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
