83d4d03f2a93a2f700e024289c44c89869b67c34
Foldster <foldsters@gmail.com>
2021-07-12 14:07:46 -0400

    Created Valve documentation file and refactored tests to match document

--- core/src/main/java/com/steamtechs/core/domain/Proportion.kt
+++ core/src/main/java/com/steamtechs/core/domain/Proportion.kt
@@ -7,15 +7,16 @@ class Proportion(var prop: Double) {
 
     init { throwIfInvalidValue(prop) }
 
-    private fun throwIfInvalidValue(p: Double) {
-        if (p < 0.0 || p > 1.0) throw InvalidProportionValueException("Proportion value must be between 0.0 and 1.0 inclusive.")
-    }
-
+    // Delegating classes require getValue and setValue methods to be defined with these arguments
     operator fun getValue(thisRef: Any?, property: KProperty<*>) = prop
 
     operator fun setValue(thisRef: Any?, property: KProperty<*>, value: Double) {
         throwIfInvalidValue(value)
         prop = value
+    }
+
+    private fun throwIfInvalidValue(p: Double) {
+        if (p < 0.0 || p > 1.0) throw InvalidProportionValueException("Proportion value must be between 0.0 and 1.0 inclusive.")
     }
 
     class InvalidProportionValueException(message: String) : Exception(message)
--- core/src/test/java/com/steamtechs/core/domain/ProportionTest.kt
+++ core/src/test/java/com/steamtechs/core/domain/ProportionTest.kt
@@ -6,14 +6,22 @@ import org.junit.jupiter.api.Assertions.*
 internal class ProportionTest {
 
     @Test
-    @DisplayName("When init to valid value, the variable equals the input")
+    @DisplayName("When delegated variable is initialized with valid input, the getter returns that input")
     fun `When init to valid value, the variable equals the input`() {
         val p: Double by Proportion(0.5)
         assertEquals(0.5, p)
     }
 
     @Test
-    @DisplayName("When init to invalid value, an error is thrown")
+    @DisplayName("When delegated variable is set with valid value, the getter returns that value")
+    fun `When set to valid value, the variable equals the input`() {
+        var p: Double by Proportion(0.5)
+        p = 0.6
+        assertEquals(0.6, p)
+    }
+
+    @Test
+    @DisplayName("When delegated variable is initialized to invalid value, an error is thrown")
     fun `When init to invalid value, an error is thrown`() {
         assertThrows<Proportion.InvalidProportionValueException> {
             var p: Double by Proportion(-0.5)
@@ -21,7 +29,7 @@ internal class ProportionTest {
     }
 
     @Test
-    @DisplayName("When valid init is set to invalid value, an error is thrown")
+    @DisplayName("When delegated variable is set to invalid value, an error is thrown")
     fun `When valid init is set to invalid value, an error is thrown`() {
         var p: Double by Proportion(0.5)
         assertThrows<Proportion.InvalidProportionValueException> {
